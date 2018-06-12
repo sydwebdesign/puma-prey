5ab668cbe208b0ef96954e75ac8ff2f62e194518
Eric Mead <eric.mead@pumascan.com>
2018-06-12 12:46:41 -0500

    second version of racoon

---
+++ Raccoon2/Models/HuntModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Puma.Prey.Raccoon.Models
+{
+    public class HuntModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
